da7bf53cc943dfd57673855b729074fe38d44842
Kos4ya <palenovkd@mail.ru>
2025-04-11 11:58:39 +0300

    New: создание таблицы reservations

---
+++ app/models/reservations.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column, String, Integer, DateTime, ForeignKey
+
+from base import Base
+
+
+class Reservation(Base):
+    __tablename__ = "reservations"
+
+    customer_name = Column(String, nullable=False)
+    table_id = Column(Integer, ForeignKey("tables.id"), nullable=False)
+    reservation_time = Column(DateTime(timezone=True), nullable=False)
+    duration_minutes = Column(Integer, nullable=False)
